535d6aa109bf775088eb7f22df029cbc88d2ee36
Isco del Collado <fedelcollado@gmail.com>
2026-07-14 12:59:05 -0600

    feat(*): practice angular [ISCO-260714]

--- src/app/pages/labs/labs.ts
+++ src/app/pages/labs/labs.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -9,4 +9,32 @@ import { Component } from '@angular/core';
 export class Labs {
   welcome = 'Hi!';
   tasks = ['Run in the park', 'Learn Angular', 'Do something casual'];
+  name = signal('Isco');
+  age = 36;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+  person = {
+    name: 'Isco',
+    age: 36,
+    avatar : this.img,
+  }
+
+  clickHandler() {
+    alert('Hi')
+  };
+
+  inputHandler(event: Event) {
+    console.log(event);
+  };
+
+  keyDownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  };
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
 }
